05fc0e33e1c221c3ef74c53f8256284787ae7a81
ArminAlonso1973 <arminalonsop@gmail.com>
2024-10-16 17:24:03 -0300

    segunda carga
    tratar de cargar

--- flask_app_.py
+++ flask_app_.py
@@ -65,4 +65,5 @@ def webhook():
 
 if __name__ == "__main__":
     # Correr la aplicación Flask en el puerto 5001
-    app.run(host='0.0.0.0', port=10000)
+    if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
